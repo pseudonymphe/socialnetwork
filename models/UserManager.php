f776004e582e3f50621772fbb2db033045b1bd60
pseudonymphe <stephcorpuz@gmail.com>
2021-04-27 14:39:37 +0200

    simple login/logout system

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,17 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($username, $password)
+{
+  global $PDO;
+  $response = $PDO->query("SELECT * FROM user where nickname = '$username' AND password = '$password'");
+  $users = $response->fetchAll();
+  $nbOfUsersWithThisPasswordAndNickname = count($users);
+
+  if ($nbOfUsersWithThisPasswordAndNickname == 1) {
+    var_dump($users);
+  } else {
+    return -1;
+  }
+}
