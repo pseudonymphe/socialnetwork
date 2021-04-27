2250816a783cef615ae8b36a45c61bbb919347c8
pseudonymphe <stephcorpuz@gmail.com>
2021-04-27 16:20:15 +0200

    use md5 for passwords

--- models/UserManager.php
+++ models/UserManager.php
@@ -18,13 +18,16 @@ function GetAllUsers()
 function GetUserIdFromUserAndPassword($username, $password)
 {
   global $PDO;
-  $response = $PDO->query("SELECT * FROM user where nickname = '$username' AND password = '$password'");
-  $users = $response->fetchAll();
-  $nbOfUsersWithThisPasswordAndNickname = count($users);
-
-  if ($nbOfUsersWithThisPasswordAndNickname == 1) {
-    $connectingUser = $users[0];
-    return $connectingUser['id'];
+  $response = $PDO->prepare("SELECT id FROM user WHERE nickname = :username AND password = MD5(:password) ");
+  $response->execute(
+    array(
+      "username" => $username,
+      "password" => $password
+    )
+  );
+  if ($response->rowCount() == 1) {
+    $row = $response->fetch();
+    return $row['id'];
   } else {
     return -1;
   }
@@ -45,7 +48,7 @@ function IsNicknameFree($nickname)
 function CreateNewUser($nickname, $password)
 {
   global $PDO;
-  $response = $PDO->prepare("INSERT INTO user (nickname, password) values (:nickname , :password )");
+  $response = $PDO->prepare("INSERT INTO user (nickname, password) values (:nickname , MD5(:password) )");
   $response->execute(
     array(
       "nickname" => $nickname,
